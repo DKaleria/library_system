9303b2227194a9c0109b828017e6451af7431190
kaleria <dybovik.val@gmail.com>
2025-02-24 13:23:42 +0300

    Added grpc server

--- auth_user-service/src/main/java/com/example/auth_userservice/service/impl/UserServiceImpl.java
+++ auth_user-service/src/main/java/com/example/auth_userservice/service/impl/UserServiceImpl.java
@@ -1,18 +1,23 @@
 package com.example.auth_userservice.service.impl;
 
+import com.example.auth_userservice.AuthUser;
+import com.example.auth_userservice.AuthUserServiceGrpc;
 import com.example.auth_userservice.config.IdentityServiceClient;
 import com.example.auth_userservice.database.entity.User;
 import com.example.auth_userservice.database.repository.UserRepository;
 import com.example.auth_userservice.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@GrpcService
 @Transactional
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends AuthUserServiceGrpc.AuthUserServiceImplBase implements UserService {
     private final UserRepository userRepository;
     private final IdentityServiceClient identityServiceClient;
 
@@ -47,5 +52,18 @@ public class UserServiceImpl implements UserService {
     @Transactional(readOnly = true)
     public User getCurrentUser(String token) {
         return identityServiceClient.getCurrentUser(token);
+    }
+
+    @Override
+    public void checkUserExists(AuthUser.CheckUserRequest request, StreamObserver<AuthUser.CheckUserResponse> responseObserver) {
+        Long userId = request.getUserId();
+        boolean exists = userRepository.findById(userId).isPresent();
+
+        AuthUser.CheckUserResponse response = AuthUser.CheckUserResponse.newBuilder()
+                .setExists(exists)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
